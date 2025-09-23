451e4d8db6cee7e631f920f8245ee6e365784c68
Ammar2521 <ammar.khalouf1021@gmail.com>
2025-09-23 06:44:39 -0700

    Add project files.

---
+++ Bankaccont.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankaccont_ovning
+{
+    public class Bankaccont
+    {
+        private int balance;
+
+
+        // Det här är en konstruktor som sätter startvärdet på balance.
+        public Bankaccont(int Balance)
+        {
+            balance = Balance;
+
+
+        }
+
+        // Det är en metod som plussar på ett värde till balance.
+        public void Deposit(int amount)
+        {
+            Console.WriteLine("Du har satt in: " + amount);
+            balance += amount;
+
+
+
+        }
+
+        // Detta är metodonen för att dra av eller uttåg från kontot.
+        public void Withdraw(int amount)
+        {
+            Console.WriteLine("Du har tagit ut: " + amount);
+            balance -= amount;
+        }
+
+        // Det här är en metod för att kolla saldot på kontot.
+        public int GetBalance()
+        {
+            return balance;
+
+
+        }
+        // Det här är en metod som visar insättning, uttåg och saldo.
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Saldo: {GetBalance()}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bankaccont_ovning
+{
+    internal class Program
+    {
+          static void Main(string[] args)
+        {
+            Bankaccont bankaccont = new Bankaccont(91);
+
+            bankaccont.Deposit(92);
+            bankaccont.Withdraw(5000);
+            bankaccont.ShowBalance();
+
+        }
+    }
+}
