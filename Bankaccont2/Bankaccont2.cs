fa55d426e907b399b003bbeab1c01c0a91454111
Ammar2521 <ammar.khalouf1021@gmail.com>
2025-09-30 05:07:01 -0700

    uppgift_4

---
+++ Bankaccont2/Bankaccont2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankaccont2
+{
+    internal class Bankaccont2
+    {
+    }
+}
---
+++ ovningarna under hela terminen/uppgift_ 4 - träningskastet/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace uppgift__4___träningskastet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Random kastet = new Random();
+           
+            // metoden av första träningen och andra täningen.
+            int förstatäingen = kastet.Next(1, 7);
+            Console.WriteLine("kasta förtsa täningen: "); 
+            Console.ReadKey();
+            Console.WriteLine($"Du kastade: {förstatäingen}");
+
+            int andratäningen = kastet.Next(1, 7);
+            Console.WriteLine("kasta andra täningen: ");
+            Console.ReadKey();
+            Console.WriteLine($"Du kastade: {andratäningen}");
+
+            Console.WriteLine($"Resultatet: {förstatäingen + andratäningen}");
+
+
+
+
+        }
+    }
+}
